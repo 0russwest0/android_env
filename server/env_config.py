10c13c4af3ffc864ce2d94737e1db3b3b2f8e34d
0russwest0 <1074124719@qq.com>
2025-08-18 11:48:49 +0800

    feat: introduce ENV_CONFIGS for Android virtual devices in env_config.py

---
+++ server/env_config.py
@@ -0,0 +1,173 @@
+ENV_CONFIGS = {
+    'default': [
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_2_xl_android_10',
+            # 'run_headless': True,
+            # 'adb_path': '/root/Android/Sdk/platform-tools/adb',
+            # 'emulator_path': '/root/Android/Sdk/emulator/emulator',
+            # 'android_sdk_root': '/root/Android/Sdk',
+            # 'android_avd_home': '/root/.android/avd'
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_2_xl_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_2_xl_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_2_xl_android_13',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3_android_13',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3a_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3a_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3a_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_3a_android_13',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_android_13',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_xl_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_xl_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_xl_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4_xl_android_13',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4a_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4a_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4a_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_4a_android_13',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_13',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_13',
+        },   
+    ],
+        'pixel_5': [
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_5_android_13',
+        },
+    ],
+    'pixel_6': [
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_10',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_11',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_12',
+        },
+        {
+            'task_config_path': '/root/android_env/tasks/dummy.textproto',
+            'avd_name': 'pixel_6_android_13',
+        },
+    ],
+}
