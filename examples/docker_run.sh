f8bc75c96d7a9319ae8944cf6cea2c5d6895e519
russwest404 <1074124719@qq.com>
2025-08-24 18:03:50 +0800

    Enhance docker_run.sh for improved Android emulator configuration
    - Added environment variables to configure the Android emulator, including paths for the emulator and ADB.
    - Included device access for KVM to enable hardware acceleration.
    
    These updates streamline the setup process for running the Android emulator in a Docker container.

--- examples/docker_run.sh
+++ examples/docker_run.sh
@@ -1,4 +1,11 @@
 docker run --rm \
     -p 5001:5001 \
     -v /root/Android/Sdk:/opt/android \
+    --device /dev/kvm \
+    -e ANDROID_ENV_AUTOLOAD=true \
+    -e ANDROID_ENV_MODE=emulator \
+    -e ANDROID_ENV_TASK_PATH=/tasks/dummy.textproto \
+    -e EMULATOR_PATH=/opt/android/emulator/emulator \
+    -e ANDROID_SDK_ROOT=/opt/android \
+    -e ADB_PATH=/opt/android/platform-tools/adb \
     android-env:test
